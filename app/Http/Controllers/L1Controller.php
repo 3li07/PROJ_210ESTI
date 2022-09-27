1c6ca2c5eb509d6adf2b9825efc42fb425c13285
3li07 <mahery.bemanantsoa@esti.mg>
2022-09-27 10:15:56 +0300

    L1 model

---
+++ app/Http/Controllers/L1Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\L1Model;
+use Illuminate\Http\Request;
+
+class L1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\L1Model  $l1Model
+     * @return \Illuminate\Http\Response
+     */
+    public function show(L1Model $l1Model)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\L1Model  $l1Model
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, L1Model $l1Model)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\L1Model  $l1Model
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(L1Model $l1Model)
+    {
+        //
+    }
+}
